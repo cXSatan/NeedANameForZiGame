31b978d05177039326a9d879fc3f3a48d1c898c2
cXSatan <ruivinho.carvalho@gmail.com>
2018-11-17 18:23:43 +0000

    Initial commit

---
+++ Assets/Rogue/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+    // Use this for initialization
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        player.AddForce(movement);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement; // tirar isto daqui isto é para o gamemanager scenemanager o que quiseres chamar
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 10;
+    public int jump = 30;
+    public int hp = 5;
+    public float stamina = 100f;
+    public Rigidbody2D playerRb;
+    public Animator playerAnim;
+    // Use this for initialization
+    int rotate;
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+        rotate = 1;
+    }
+
+    private void FixedUpdate() //tudo o que for com fisica e aqui relativamente ao playa
+    {
+       playerAnim.SetBool("IsRunning", false);
+       
+
+        if (Input.GetKey("right")) {
+
+            if (rotate == 0)
+            {
+                transform.Rotate(new Vector3(0, 180, 0));
+                rotate = 1;
+            }
+           playerAnim.SetBool("IsRunning", true);
+            playerRb.AddForce(new Vector2(speed, 0));
+        }
+
+
+        if (Input.GetKey("left")) {
+            
+            if (rotate == 1) { 
+                transform.Rotate(new Vector3 (0,-180,0));
+                rotate = 0;
+            }
+            playerRb.AddForce(new Vector2(-speed, 0));
+            playerAnim.SetBool("IsRunning", true);
+        }
+
+
+        if (Input.GetKey(KeyCode.Space)) {
+            playerAnim.SetBool("IsRunning", false);
+            playerRb.AddForce(new Vector2(0, jump * Time.deltaTime),ForceMode2D.Impulse);
+
+
+
+        }
+
+        
+
+    }
+
+}
